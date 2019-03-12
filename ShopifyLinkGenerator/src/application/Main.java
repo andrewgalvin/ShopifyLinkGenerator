dae45678a03c69b82091071bdd0a28ed34616a77
Andrew Galvin <andrewggalvin@gmail.com>
2019-03-12 19:25:42 -0400

    Added code.

--- ShopifyLinkGenerator/src/application/Main.java
+++ ShopifyLinkGenerator/src/application/Main.java
@@ -1,26 +1,103 @@
 package application;
 	
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Main extends Application implements Initializable{
+	
+	@FXML
+	private TextField productTextField;
+	
+	@FXML
+	private TextField sizeTextField;
+	
+	@FXML
+	private TextField atcTextField; 
+	
+	@FXML
+	private Button createButton;
+	
+	@FXML
+	private Button openButton;	
+	
+	String url;// = "https://kith.com/collections/mens-footwear-sneakers/products/new-balance-ml1500v1-navy-grey";
+	String atcURL;
+	String size;
+	String prodID;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("LinkGenGUI.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();//a
+			primaryStage.setResizable(false);
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	public void getProduct() throws IOException {
+		Document doc = Jsoup.connect(url).get();
+		Elements elements = doc.select("select.no-js > option");
+		prodID = elements.toString();
+		prodID = prodID.substring(prodID.indexOf(size)-16,prodID.indexOf(size)-2);
+		System.out.println(prodID);
+	}
+	
+	public void setATC() {
+		atcURL = url.substring(0,url.indexOf(".")+4);
+		atcURL = atcURL + "/cart/" + prodID + ":1";
+		atcTextField.setText(atcURL);
+	}
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		createButton.setOnMouseClicked(e -> {
+			size = sizeTextField.getText();
+			size = size.toUpperCase();
+			size = " " + size + " ";
+			url = productTextField.getText();
+			try {
+				getProduct();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			setATC();
+			System.out.println(atcURL);
+		});
+		
+		openButton.setOnMouseClicked(e -> {
+			try {
+				Desktop.getDesktop().browse(new URL(atcURL).toURI());
+			}catch(Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+		
 	}
 }
